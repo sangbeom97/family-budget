3e26d04cd9bfe174c6d81843c347a640e3f9350a
sangbeom97 <sangbeom0301@gmail.com>
2026-06-04 15:54:41 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2004,9 +2004,9 @@ value
           <YAxis />
 <Tooltip
   formatter={(value) => [
-    `%${Number(
+    `%{Number(
       value
-    ).toLocaleString()}`,
+    ).toLocaleString()}%`,
     " ",
   ]}
 />
@@ -2038,9 +2038,9 @@ value
           <YAxis />
 <Tooltip
   formatter={(value) => [
-    `%${Number(
+    `${Number(
       value
-    ).toLocaleString()}`,
+    ).toLocaleString()}%`,
     " ",
   ]}
 />
