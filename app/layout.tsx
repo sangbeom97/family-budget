dc5d8a34af45fc210d1bdc9f011f28b0d747f531
sangbeom97 <sangbeom0301@gmail.com>
2026-06-18 11:26:31 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,15 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+// subsets의 이중 배열 괄호[[]] 문제를 해결했습니다.
 const geistSans = Geist({
   variable: "--font-geist-sans",
-  subsets: [["latin"]],
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
-  subsets: [["latin"]],
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
