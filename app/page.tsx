a327f63448acc8e31a92809ab853df28d2d1d714
sangbeom97 <sangbeom0301@gmail.com>
2026-07-01 16:41:55 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -204,7 +204,7 @@ export default function Home() {
     const { data, error } = await supabase
   .from("group_members")
   .select("*")
-  .eq("user_id", session?.user?.id);
+  .eq("user_id", user.id);
 
 console.log("GROUP DATA =", data);
 console.log("GROUP ERROR =", error);
